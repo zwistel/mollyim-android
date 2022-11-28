5eba7398cea3a07c867a026ce1858781cba6cd5b
sim <git@sgougeon.fr>
2022-11-28 21:38:13 +0100

    Add UnifiedPush Refresh Job

--- app/src/main/java/org/thoughtcrime/securesms/ApplicationContext.java
+++ app/src/main/java/org/thoughtcrime/securesms/ApplicationContext.java
@@ -123,6 +123,7 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
+import im.molly.unifiedpush.jobs.UnifiedPushRefreshJob;
 import io.reactivex.rxjava3.exceptions.OnErrorNotImplementedException;
 import io.reactivex.rxjava3.exceptions.UndeliverableException;
 import io.reactivex.rxjava3.plugins.RxJavaPlugins;
@@ -198,7 +199,6 @@ public class ApplicationContext extends MultiDexApplication implements AppForegr
                             .addBlocking("network-settings", this::initializeNetworkSettings)
                             .addBlocking("first-launch", this::initializeFirstEverAppLaunch)
                             .addBlocking("gcm-check", this::initializeFcmCheck)
-                            .addBlocking("unifiedpush", UnifiedPushHelper::initializeUnifiedPush)
                             .addBlocking("app-migrations", this::initializeApplicationMigrations)
                             .addBlocking("lifecycle-observer", () -> ApplicationDependencies.getAppForegroundObserver().addListener(this))
                             .addBlocking("message-retriever", this::initializeMessageRetrieval)
@@ -484,7 +484,7 @@ public class ApplicationContext extends MultiDexApplication implements AppForegr
     }
   }
 
-  private void initializeFcmCheck() {
+  public void initializeFcmCheck() {
     if (!SignalStore.account().isRegistered()) {
       return;
     }
@@ -493,6 +493,10 @@ public class ApplicationContext extends MultiDexApplication implements AppForegr
 
     if (UnifiedPushHelper.isUnifiedPushAvailable()
         || fcmStatus == PlayServicesUtil.PlayServicesStatus.DISABLED) {
+      if (!SignalStore.unifiedpush().getAirGaped()) {
+        ApplicationDependencies.getJobManager().add(new UnifiedPushRefreshJob());
+      }
+      ApplicationDependencies.getJobManager().cancel(new FcmRefreshJob().getId());
       if (SignalStore.account().isFcmEnabled()) {
         Log.i(TAG, "Play Services are disabled. Disabling FCM.");
         SignalStore.account().setFcmEnabled(false);
@@ -507,10 +511,12 @@ public class ApplicationContext extends MultiDexApplication implements AppForegr
                SignalStore.account().getFcmTokenLastSetTime() < 0) {
       Log.i(TAG, "Play Services are newly-available. Updating to use FCM.");
       SignalStore.account().setFcmEnabled(true);
+      ApplicationDependencies.getJobManager().cancel(new UnifiedPushRefreshJob().getId());
       ApplicationDependencies.getJobManager().startChain(new FcmRefreshJob())
                                              .then(new RefreshAttributesJob())
                                              .enqueue();
     } else {
+      ApplicationDependencies.getJobManager().cancel(new UnifiedPushRefreshJob().getId());
       long nextSetTime = SignalStore.account().getFcmTokenLastSetTime() + TimeUnit.HOURS.toMillis(6);
 
       if (SignalStore.account().getFcmToken() == null || nextSetTime <= System.currentTimeMillis()) {
